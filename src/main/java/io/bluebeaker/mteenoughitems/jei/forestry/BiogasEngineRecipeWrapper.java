4dba2f309f2989d830fec10a339668b8079fbda0
blue-beaker <yyy5566778899@126.com>
2025-09-28 00:09:57 +0800

    Show an alert for biogas engine recipes with constant lava consumption

--- src/main/java/io/bluebeaker/mteenoughitems/jei/forestry/BiogasEngineRecipeWrapper.java
+++ src/main/java/io/bluebeaker/mteenoughitems/jei/forestry/BiogasEngineRecipeWrapper.java
@@ -1,5 +1,6 @@
 package io.bluebeaker.mteenoughitems.jei.forestry;
 
+import forestry.core.config.Constants;
 import io.bluebeaker.mteenoughitems.jei.generic.FluidPowerRecipeWrapper;
 import io.bluebeaker.mteenoughitems.utils.RenderUtils;
 import mezz.jei.api.IJeiHelpers;
@@ -39,5 +40,14 @@ public class BiogasEngineRecipeWrapper extends FluidPowerRecipeWrapper {
         yPos += minecraft.fontRenderer.FONT_HEIGHT + 2;
 
         RenderUtils.drawTextAlignedLeft(this.energy+getPowerUnit()+"/mB", xPos, yPos, Color.gray.getRGB());
+
+        int minHeatGain = Constants.ENGINE_BRONZE_HEAT_GENERATION_ENERGY - dissipationMultiplier;
+        int maxHeatGain = Constants.ENGINE_BRONZE_HEAT_GENERATION_ENERGY * 3 - dissipationMultiplier*2;
+
+        float minLoss = maxHeatGain<0 ? (float) -Constants.ENGINE_HEAT_VALUE_LAVA /maxHeatGain : 0;
+        float maxLoss = maxHeatGain<0 ? (float) -Constants.ENGINE_HEAT_VALUE_LAVA /minHeatGain : 0;
+        if(maxLoss>0 || minLoss>0){
+            RenderUtils.drawTextAlignedLeft("!", recipeWidth-7, recipeHeight/2-8, Color.red.getRGB());
+        }
     }
 }
